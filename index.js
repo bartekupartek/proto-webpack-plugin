85c875af6a85fdcb002a5b74795c2a9adc56c949
Bartlomiej Rozanski <ebartlomiej@gmail.com>
2017-11-07 12:52:34 +0100

    add extension to file names to match

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var pbjs = require('protobufjs/cli/pbjs'),
     glob = require('glob')
 
 function ProtoWebpackPlugin(options) {
-  var pattern = options.matchFiles ? '@(' + options.matchFiles.join('|') + ')' : '*.proto'
+  var pattern = options.matchNames && '@(' + options.matchNames.join('.proto|') + '.proto)' || '*.proto'
   var files = glob.sync(pattern, { cwd: options.resolveDir, absolute: true, matchBase: true })
   pbjs.main(['-t', 'json-module', '-w', 'commonjs', '-o', options.outputFile].concat(files), function(err, output) {
     if (err) { throw err; }
